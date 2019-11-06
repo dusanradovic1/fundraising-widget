e2e49dff234fe84192798715e1e7ee6be1706f57
Dusan Radovic <dus91@yahoo.com>
2019-11-06 11:35:13 +0100

    Added behaviour for message and added CSS

--- widget.js
+++ widget.js
@@ -1,39 +1,39 @@
 window.onload = function () {
-   init();
+    goalAchieved = false;
+    progressBarValue = Number(document.getElementById("fundraise_progressBar").offsetWidth);
+    founded = Number(document.getElementById("fundraise_currentFundingText").innerHTML);
+    setInputFilter(document.getElementById("fundraise_amount"), function (value) {
+        return /^\d*$/.test(value)
+    });
+    setStates();
 };
 
-function init() {
-    fundraise = document.getElementById("fundraise_amount").value;
-    progressBarValue = Number(document.getElementById("fundraise_progressBar").offsetWidth);
-    console.log(progressBarValue);
-    progressBarPercentValue = getPercentOfValueOf(progressBarValue, 250);
-    console.log('percent of 250 is ' + progressBarPercentValue);
-    valueOfProgressBar = Math.round(getValueOfPercent(progressBarPercentValue, 1000));
-    console.log(valueOfProgressBar);
-
-    document.getElementById("fundraise_currentFundingText").innerHTML = valueOfProgressBar;
-    document.getElementById("fundraise_remainingText").innerHTML = 1000 - valueOfProgressBar;
+function setStates() {
+    progressBarPercentValue = getPercentOfValueOf(this.progressBarValue, 250);
+    this.founded = Math.round(getValueOfPercent(progressBarPercentValue, 1000));
+    document.getElementById("fundraise_amount").value = '';
+    document.getElementById("fundraise_currentFundingText").innerHTML = this.founded;
+    document.getElementById("fundraise_remainingText").innerHTML = Math.round(getPercentOfValueOf(this.founded, 1000)) + '%';
+    hideNotification(true);
+    if (this.founded === 1000) {
+        goalIsRised();
+    }
 }
 
-function myFunction() {
-    var progressBarValue = Number(document.getElementById("fundraise_progressBar").offsetWidth);
-    if (progressBarValue > 249) {
-        return;
-    }
-    var fundraise = document.getElementById("fundraise_amount").value;
-    raiseInPercents = getPercentOfValueOf(Number(fundraise), 1000);
-    console.log('Input is'+raiseInPercents+'% of 1000');
+function pledge() {
+    var fundraise = Number(document.getElementById("fundraise_amount").value);
+    raiseInPercents = getPercentOfValueOf(fundraise, 1000);
 
-    // var raiseBy = (raiseInPercents / 100) * 250;
     var raiseBy = getValueOfPercent(raiseInPercents, 250);
-    console.log(raiseBy);
-    progressBarValue += raiseBy;
-    console.log('progres' + progressBarValue);
-    if (progressBarValue > 250) {
-        progressBarValue = 250;
+    this.progressBarValue += raiseBy;
+    if (this.progressBarValue > 250) {
+        alert('amount of money that you pledged is more then it is required');
+        this.progressBarValue = 250;
     }
-    document.getElementById("fundraise_progressBar").style.width = progressBarValue + 'px';
-    init();
+    document.getElementById("fundraise_progressBar").style.width = this.progressBarValue + 'px';
+    setStates();
+    hidePledgeButtons();
+    hideNotification(false);
 }
 
 function getPercentOfValueOf(percent, percentOf) {
@@ -42,5 +42,38 @@ function getPercentOfValueOf(percent, percentOf) {
 
 function getValueOfPercent(percent, maxValue) {
     return (percent / 100) * maxValue;
+}
+
+function setInputFilter(textbox, inputFilter) {
+    ["input", "keydown", "keyup", "mousedown", "mouseup", "select", "contextmenu", "drop"].forEach(function (event) {
+        textbox.addEventListener(event, function () {
+            if (inputFilter(this.value)) {
+                this.oldValue = this.value;
+                this.oldSelectionStart = this.selectionStart;
+                this.oldSelectionEnd = this.selectionEnd;
+            } else if (this.hasOwnProperty("oldValue")) {
+                this.value = this.oldValue;
+                this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
+            }
+        });
+    });
+}
+
+function goalIsRised() {
+    document.getElementById('fundraise_progressBar').style.backgroundColor = '#1CBC2C';
+    hidePledgeButtons();
+    setTimeout(() => {
+        alert('all money is gathered');
+    },500)
+}
+
+function hidePledgeButtons() {
+    document.getElementById('fundraise_pledgeButton').hidden = true;
+    document.getElementById('fundraise_amount').hidden = true;
+}
+
+function hideNotification(hide) {
+    var [notification] = document.getElementsByClassName('notification notification-success');
+    notification.hidden = hide;
 }
 
